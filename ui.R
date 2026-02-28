525474062cf40a6c0b7a93aaf06cdf0fa4ed2c49
Conor Tompkins <conorotompkins@gmail.com>
2026-02-27 20:54:07 -0500

    reorganized nav_panels under nav_menus. renamed panels

--- ui.R
+++ ui.R
@@ -13,30 +13,59 @@ ui <- page_navbar(
 
   nav_spacer(),
 
-  nav_panel(
+  nav_menu(
     "Breeding Calendar",
+    nav_panel(
+      "Calendar",
 
-    card(reactableOutput(outputId = "calendar"))
+      card(reactableOutput(outputId = "calendar"))
+    ),
+
+    nav_panel(
+      "Safe Dates",
+
+      card(reactableOutput("dates_table"))
+    ),
+
+    nav_panel(
+      "Code Glossary",
+
+      reactableOutput("glossary_table")
+    )
   ),
 
-  nav_panel(
-    "Breeding Safe Dates",
+  nav_menu(
+    "Block Progress",
+    nav_panel(
+      "Map",
 
-    card(reactableOutput("dates_table"))
-  ),
+      layout_columns(
+        radioGroupButtons(
+          inputId = "season_variable",
+          label = "Season",
+          choices = c(
+            "All seasons" = "All seasons",
+            "Breeding" = "Breeding",
+            "Winter" = "Winter"
+          )
+        ),
+        radioGroupButtons(
+          inputId = "block_variable",
+          label = "Variable",
+          choices = c(
+            "Effort hours" = "duration_hours",
+            "Confirmed species" = "Confirmed"
+          )
+        )
+      ),
+      maplibreOutput("block_effort_map")
+    ),
 
-  nav_panel(
-    "Breeding Code Glossary",
+    nav_panel(
+      "Table",
 
-    reactableOutput("glossary_table")
-  ),
-
-  nav_panel(
-    "Block Progress Map",
-
-    layout_columns(
       radioGroupButtons(
-        inputId = "season_variable",
+        inputId = "season_variable_table",
         label = "Season",
         choices = c(
           "All seasons" = "All seasons",
@@ -44,37 +73,14 @@ ui <- page_navbar(
           "Winter" = "Winter"
         )
       ),
-      radioGroupButtons(
-        inputId = "block_variable",
-        label = "Variable",
-        choices = c(
-          "Effort hours" = "duration_hours",
-          "Confirmed species" = "Confirmed"
-        )
-      )
+      reactableOutput("block_completion_table")
     ),
-    maplibreOutput("block_effort_map")
-  ),
 
-  nav_panel(
-    "Block Progress Table",
-
-    radioGroupButtons(
-      inputId = "season_variable_table",
-      label = "Season",
-      choices = c(
-        "All seasons" = "All seasons",
-        "Breeding" = "Breeding",
-        "Winter" = "Winter"
-      )
-    ),
-    reactableOutput("block_completion_table")
-  ),
-
-  nav_panel(
-    "Block-Atlas Comparison Table",
-    textInput(inputId = "block_choice", label = "Choose Block"),
-    reactableOutput("block_atlas_comparison_table")
+    nav_panel(
+      "Atlas Comparison Table",
+      textInput(inputId = "block_choice", label = "Choose Block"),
+      reactableOutput("block_atlas_comparison_table")
+    )
   ),
 
   nav_panel(
