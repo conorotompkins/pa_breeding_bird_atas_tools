cdc19fa9281a6e499c7618d0810546d257ab86d5
Conor Tompkins <conorotompkins@gmail.com>
2026-04-07 09:12:18 -0400

    created functions to summarize for block report

---
+++ R/summarize_effort.R
@@ -0,0 +1,18 @@
+summarize_effort <- function(x) {
+  x |>
+    dplyr::select(
+      duration_hours_total,
+      duration_hours_diurnal,
+      duration_hours_nocturnal,
+      duration_hours_unknown
+    ) |>
+    rename(
+      Total = duration_hours_total,
+      Diurnal = duration_hours_diurnal,
+      Nocturnal = duration_hours_nocturnal,
+      Unknown = duration_hours_unknown
+    ) |>
+    pivot_longer(everything(), names_to = "Effort type", values_to = "Value") |>
+    mutate(Value = round(Value, 2)) |>
+    gt()
+}
